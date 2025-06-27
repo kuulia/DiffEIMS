80afdad032179ce1f9161579dfbc5a409d4f8539
kuulia <linus.lind00@gmail.com>
2025-06-27 13:23:45 +0300

    Test with more epochs

--- src/slurm/train_model.sh
+++ src/slurm/train_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_diffms
 #SBATCH --output=%A_%a.out
-#SBATCH --time=72:00:00
+#SBATCH --time=36:00:00
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:h200:1
