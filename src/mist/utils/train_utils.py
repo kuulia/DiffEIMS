0ccb7cbe50a29c254f8688d8b13ffa59a3582f18
Kuulia <linus.lind00@gmail.com>
2025-08-04 11:03:47 +0300

    move to src/utils.py

--- src/mist/utils/train_utils.py
+++
@@ -1,9 +0,0 @@
-""" train_utils.py"""
-import os
-from typing import NoReturn
-
-
-def make_result_dirs(dirs_to_create: list[str]) -> NoReturn:
-
-    for path in dirs_to_create:
-        os.makedirs(path, exist_ok=True)
